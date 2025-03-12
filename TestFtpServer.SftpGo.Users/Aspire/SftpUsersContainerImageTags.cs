8fd0b7ab898ce0ac3ffac1de48a78456d937d25e
Peter LaComb <Peter.LaComb@sscinc.com>
2025-03-12 14:14:11 -0400

    feat: Tag image correctly

--- TestFtpServer.SftpGo.Users/Aspire/SftpUsersContainerImageTags.cs
+++ TestFtpServer.SftpGo.Users/Aspire/SftpUsersContainerImageTags.cs
@@ -3,6 +3,6 @@ namespace TestFtpServer.SftpGo.Users.Aspire;
 internal static class SftpUsersContainerImageTags
 {
     internal const string Registry = "";
-    internal const string Image = "sftpgousers";
+    internal const string Image = "peterlacombssnc/sftpgousers";
     internal const string Tag = "latest";
 }
